99051fe91e1a47d941d23cbc07c672699b939149
kapil <kapil.personal9711@gmail.com>
2024-03-01 18:25:16 +0530

    added backward animation on postion and aligned text to center in gallery section

--- javascript/index.js
+++ javascript/index.js
@@ -5,11 +5,23 @@ const contentImgWrapper = document.querySelector(
   ".about__img-and-content-wrapper"
 );
 
+const firstContainer = document.querySelector(
+  ".common-container:nth-of-type(1)"
+);
+const thirdContainer = document.querySelector(
+  ".common-container:nth-of-type(3)"
+);
+
 // accessing distance from top
 
 const { top: topDistanceH2 } = aboutH2.getBoundingClientRect();
 const { top: topDistanceImgContent } =
   contentImgWrapper.getBoundingClientRect();
+
+const { top: firstContainerTopDistance } =
+  firstContainer.getBoundingClientRect();
+const { top: thirdContainerTopDistance } =
+  thirdContainer.getBoundingClientRect();
 
 //   listning to scroll
 
@@ -19,20 +31,40 @@ window.addEventListener("scroll", () => {
   const { top: topDistanceImgContent } =
     contentImgWrapper.getBoundingClientRect();
 
+  const { top: firstContainerTopDistance } =
+    firstContainer.getBoundingClientRect();
+  const { top: thirdContainerTopDistance } =
+    thirdContainer.getBoundingClientRect();
+
   // getting the height of the client on scrolling
   let boxHeight = parseFloat(contentImgWrapper.clientHeight);
+  let firstContainerHeight = parseFloat(firstContainer.clientHeight);
   let windowHeight = parseFloat(window.innerHeight);
   console.log(windowHeight, boxHeight);
 
   if (parseInt(topDistanceH2) < 10000) aboutH2.classList.add("originate");
   else aboutH2.classList.remove("originate");
 
-  if (windowHeight - topDistanceImgContent >= parseFloat(boxHeight / 2 - 50)) {
+  console.log(topDistanceImgContent);
+  if (
+    windowHeight - topDistanceImgContent >= parseFloat(boxHeight / 2 - 50) &&
+    topDistanceImgContent > -parseFloat(boxHeight)
+  ) {
     imgWrapper.classList.add("originate");
     contentWrapper.classList.add("originate");
   } else {
     imgWrapper.classList.remove("originate");
     contentWrapper.classList.remove("originate");
+  }
+  if (
+    windowHeight - firstContainerTopDistance >=
+    parseFloat(firstContainerHeight / 2 - 50)
+  ) {
+    firstContainer.classList.add("originate");
+    thirdContainer.classList.add("originate");
+  } else {
+    firstContainer.classList.remove("originate");
+    thirdContainer.classList.remove("originate");
   }
 });
 
